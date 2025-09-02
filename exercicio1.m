3b76f507c8c1b837ffe8712358487e231fc04873
GustavoFrug <gustavo.frug.mauro@gmail.com>
2025-09-02 12:07:02 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -9,18 +9,17 @@ imax = 20;
 
 t_roots = zeros(imax,1);
 t_roots(1) = x0;
-erro = zeros(lenght(t)-1);
 
-for m = 1:length(t_roots) - 1
-    if m ~= 1
-        erro(m) = abs((t_roots(m)-t_roots(m-1))/t_roots(m))
-        if erro(m) < es
+for ii = 1:length(t_roots) - 1
+    if ii ~= 1
+        erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii))
+        if erro(ii) < es
             break
         endif
     endif
-        t_roots(m+1) = t_roots(m) - func(t_roots(m))/func_d(t_roots(m));
+        t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii));
     endfor
-t = t_roots(m)
+t = t_roots(ii)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
